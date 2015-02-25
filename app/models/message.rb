7bc1271ae414c5ace344f1311a7b1d137fd19258
Ivan Puchkov <rake.eclipse@gmail.com>
2015-02-25 12:37:25 +0600

    add searchable block to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,16 @@ class Message < ActiveRecord::Base
 
   scope :ordered, -> { order('created_at') }
 
+  searchable do
+    text :subject
+    text :contacts, :using => :contacts_values
+    string :created_at
+  end
+
+  def contacts_values
+    contacts.map(&:value).join(' ')
+  end
+
   def delivery_status_text_for(contact)
     contact_messages.find_by(:contact_id => contact.id).status_text
   end
