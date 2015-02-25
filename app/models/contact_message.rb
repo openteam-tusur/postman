fc409b175c3fb96b493fb5a6d5af3e09359db817
Ivan Puchkov <rake.eclipse@gmail.com>
2015-02-25 15:01:15 +0600

    change contact_message statuses

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,7 +1,7 @@
 class ContactMessage < ActiveRecord::Base
   extend Enumerize
 
-  enumerize :status, :in => [:sended, :delivered, :undelivered], :default => :sended, :predicates => true
+  enumerize :status, :in => [:initialized, :sended, :received, :delivered, :failed], :default => :initialized, :predicates => true
 
   belongs_to :contact
   belongs_to :message
