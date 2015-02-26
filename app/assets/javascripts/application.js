2a59f5d2a5c984040589fc888a97184b3c1d7396
tab <tukhfatullin@gmail.com>
2015-02-26 11:39:29 +0600

    Simple message view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap/transition
+//= require bootstrap/tab
 //= require tusur_header
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def delivery_status(message, contact)
+    message.contact_messages.find_by(:contact_id => contact.id).status
+  end
 end
