ca2e15ecfb794601f2d6e9f04606ecee97a860a4
tab <tukhfatullin@gmail.com>
2015-02-26 15:52:33 +0600

    Add document and page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def document_title
+    [page_title, I18n.t('site')].compact.join(' / ')
+  end
+
+  def page_title(title = nil)
+    @page_title ||= I18n.t("page_title.#{controller_name}.#{action_name}", :title => title).html_safe
+  end
+
   def delivery_status(message, contact)
     message.contact_messages.find_by(:contact_id => contact.id).status
   end
--- config/navigation.rb
+++ config/navigation.rb
@@ -4,8 +4,8 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :main_page,      I18n.t("page_title.main_page.index"), root_path
     primary.item :messages,       I18n.t("page_title.messages.index"),  messages_path
-    primary.item :email_property, I18n.t("page_title.email_property.index"),  property_emails_path
-    primary.item :sms_property,   I18n.t("page_title.sms_property.index"),  property_smses_path
+    primary.item :email_property, I18n.t("page_title.emails.index"),  property_emails_path
+    primary.item :sms_property,   I18n.t("page_title.smses.index"),  property_smses_path
   end
 end
 
