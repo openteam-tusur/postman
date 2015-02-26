0616b852e509752e948dcfc36c8eda03dadb4bc9
tab <tukhfatullin@gmail.com>
2015-02-26 12:32:20 +0600

    Use message properties in email template

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,8 +1,9 @@
 class MessageMailer < CommonMailer
-  def send_email(subject, body, recipient)
+  def send_email(subject, body, recipient, property)
     @subject   = subject
     @body      = body
     @recipient = recipient
+    @property  = property
 
     mail(:from => 'fake@fake.fake', :to => recipient, :subject => subject)
   end
