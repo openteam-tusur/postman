231aa9d404306a2e5e7d07da4b7c99feb7ebace3
tab <tukhfatullin@gmail.com>
2015-02-26 12:11:06 +0600

    Add navigation and page_titles for email and sms

--- config/navigation.rb
+++ config/navigation.rb
@@ -2,8 +2,10 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
 
   navigation.items do |primary|
-    primary.item :main_page, I18n.t("page_title.main_page.index"), root_path
-    primary.item :messages,  I18n.t("page_title.messages.index"),  messages_path
+    primary.item :main_page,      I18n.t("page_title.main_page.index"), root_path
+    primary.item :messages,       I18n.t("page_title.messages.index"),  messages_path
+    primary.item :email_property, I18n.t("page_title.email_property.index"),  property_emails_path
+    primary.item :sms_property,   I18n.t("page_title.sms_property.index"),  property_smses_path
   end
 end
 
