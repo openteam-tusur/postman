577c062200d78a6a74a4aa599c9bc0c5c2f70c5c
tab <tukhfatullin@gmail.com>
2015-02-27 14:51:46 +0600

    Add sender email address for Property::Email

--- app/models/property/email.rb
+++ app/models/property/email.rb
@@ -1,5 +1,5 @@
 class Property::Email < Property
   has_many :messages, :class_name => 'Message::Email', :foreign_key => :property_id
 
-  validates_presence_of :url, :footer
+  validates_presence_of :url, :footer, :email
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227040458) do
+ActiveRecord::Schema.define(version: 20150227084638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150227040458) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
+    t.string   "email"
   end
 
   add_foreign_key "contact_messages", "contacts"
