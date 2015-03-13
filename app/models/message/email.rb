c61966a9b6d6487c1f88a070a1a306d3e4c64bbe
Manin Ilya <bkmzvf@gmail.com>
2015-03-13 17:02:18 +0600

    replace validations in models

--- app/models/message/email.rb
+++ app/models/message/email.rb
@@ -1,7 +1,7 @@
 class Message::Email < Message
-  validates_presence_of :body
+  validates_presence_of :subject, :body
 
   def send_to(recipients)
-    recipients.each { |recipient| MessageMailer.delay.send_email(self, recipient) }
+    recipients.each { |recipient| MessageMailer.delay.send_email(subject, body, recipient.value, recipient.contact_message_id, property) }
   end
 end
