30555798fd1d62e8a6500077df11ec38779b7699
Manin Ilya <bkmzvf@gmail.com>
2015-03-20 09:17:17 +0600

    fix indent

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -4,10 +4,10 @@ class MessageMailer < ActionMailer::Base
   layout 'email'
 
   def send_email(subject, body, recipient, contact_message_id = nil, property)
-    @body   = body
+    @body     = body
     @property = property
 
-    headers['X-MC-Metadata'] = { :contact_message_id => contact_message_id}.to_json if contact_message_id
+    headers['X-MC-Metadata'] = { :contact_message_id => contact_message_id }.to_json if contact_message_id
 
     mail(:from => property.email, :to => recipient, :subject => subject)
   end
