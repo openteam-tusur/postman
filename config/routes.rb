b84737145c6083119c89cb529142841f0f3cb733
Manin Ilya <bkmzvf@gmail.com>
2015-03-24 12:36:21 +0600

    open sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
+  mount Sidekiq::Web => '/sidekiq'
   mount Api => '/'
 
   resources :messages, :only => [:index, :show]
