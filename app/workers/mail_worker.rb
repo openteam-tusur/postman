25379b09e6531e8d5138a9362c195c5637f796a5
Evgeniy Lapin <lev@openteam.ru>
2018-02-21 14:58:51 +0700

    updated some gems

--- app/workers/mail_worker.rb
+++ app/workers/mail_worker.rb
@@ -1,14 +1,16 @@
 class MailWorker
   include Sidekiq::Worker
 
+  sidekiq_options retry: true
+
   def perform(subject, body, emails_with_uuid, slug)
-    emails = emails_with_uuid.map { |email, _| Contact::Email.find_or_create_by :value => email }
-    property = Property::Email.find_by(:slug => slug)
+    emails = emails_with_uuid.map { |email, _| Contact::Email.find_or_create_by value: email }
+    property = Property::Email.find_by(slug: slug)
     return unless property.present?
-    message = property.messages.create :subject => subject, :body => body
+    message = property.messages.create subject: subject, body: body
 
     emails.each do |email|
-      message.contact_messages.find_or_create_by :contact_id => email.id, :uuid => emails_with_uuid[email.value]
+      message.contact_messages.find_or_create_by contact_id: email.id, uuid: emails_with_uuid[email.value]
     end
 
     SendMessage.new(message).process
