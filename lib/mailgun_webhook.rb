dd3cdf5e1d50295e7b5376895650c7a7071c4112
Evgeniy Lapin <lev@openteam.ru>
2018-06-30 01:06:31 +0700

    another fix

--- lib/mailgun_webhook.rb
+++ lib/mailgun_webhook.rb
@@ -3,13 +3,6 @@ class MailgunWebhook
     @hash = hash
     @contact_message = ContactMessage.find(@hash['contact_message_id']) rescue nil
     @event = @hash['event']
-    Rails.logger.info '='*100
-    Rails.logger.info @hash
-    Rails.logger.info @hash.inspect
-    Rails.logger.info @contact_message
-    Rails.logger.info @contact_message.inspect
-    Rails.logger.info @event
-    Rails.logger.info '='*100
   end
 
   def perform
@@ -18,22 +11,22 @@ class MailgunWebhook
     case @event
     when 'delivered'
       @contact_message.update_attributes!(
-        raw_email_status: @hash.serialized_hash,
+        raw_email_status: @hash.to_json,
         status: :received
       )
     when 'opened' || 'clicked'
       @contact_message.update_attributes!(
-        raw_email_status: @hash.serialized_hash,
+        raw_email_status: @hash.to_json,
         status: :delivered
       )
     when 'dropped' || 'bounced'
       @contact_message.update_attributes!(
-        raw_email_status: @hash.serialized_hash,
+        raw_email_status: @hash.to_json,
         status: :failed
       )
     when 'complained' || 'unsubscribed'
       @contact_message.update_attributes!(
-        raw_email_status: @hash.serialized_hash,
+        raw_email_status: @hash.to_json,
         status: :spam
       )
     end
