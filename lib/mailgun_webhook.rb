437b03e0a8cbef4ddcce8ea521908868877097bb
Evgeniy Lapin <lev@openteam.ru>
2018-06-29 23:57:34 +0700

    fix variables

--- lib/mailgun_webhook.rb
+++ lib/mailgun_webhook.rb
@@ -6,26 +6,26 @@ class MailgunWebhook
   end
 
   def perform
-    return if contact_message.blank? || event.blank?
+    return if @contact_message.blank? || @event.blank?
 
-    case event
+    case @event
     when 'deliver'
-      contact_message.update_attributes!(
+      @contact_message.update_attributes!(
         raw_email_status: hash.serialized_hash,
         status: :received
       )
     when 'open' || 'click'
-      contact_message.update_attributes!(
+      @contact_message.update_attributes!(
         raw_email_status: hash.serialized_hash,
         status: :delivered
       )
     when 'bounce' || 'spam_complaint' || 'policy_rejection' || 'generation_failure' || 'generation_rejection'
-      contact_message.update_attributes!(
+      @contact_message.update_attributes!(
         raw_email_status: hash.serialized_hash,
         status: :failed
       )
     when 'spam' || 'unsubscribe'
-      contact_message.update_attributes!(
+      @contact_message.update_attributes!(
         raw_email_status: hash.serialized_hash,
         status: :spam
       )
