71e813ebcad122bb009bbf1bb18021b68f13fc5c
Evgeniy Lapin <lev@openteam.ru>
2019-03-30 15:55:48 +0700

    updated session store settings

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :redis_session_store, :serializer => :json, :redis => Settings['session_store'].to_hash, :domain => Settings['app.domain']
+Rails.application.config.session_store :redis_session_store,
+  serializer: :json,
+  redis: Settings['session_store'].to_hash.merge!(expire_after: 7.day),
+  domain: Settings['app.domain']
